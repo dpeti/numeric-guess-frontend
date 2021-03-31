a7dd1ab463e05dd1b36f6d5e8c2759944ebb5843
dergecz.peter <dergecz.peter@innoteq.hu>
2021-03-31 22:49:27 +0200

    Play a game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
 import { HomeComponent } from './component/home/home.component';
 import { GameComponent } from './component/game/game.component';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/game/game.component.ts
+++ src/app/component/game/game.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {GameService} from '../../service/game.service';
+import {Subscription} from 'rxjs';
+import {GameDto} from '../../model/game.dto';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ResultDto} from '../../model/result.dto';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  private gameSubs: Subscription;
+  private gameCheckSubs: Subscription;
+  private paramsSubs: Subscription;
+
+  game: GameDto;
+  result: ResultDto = new ResultDto();
+  gameId: string;
+
+  number: number;
+
+  lower: boolean;
+  greater: boolean;
+  equal: boolean;
+
+  constructor(public gameService: GameService,
+              private activedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.paramsSubs = this.activedRoute.params.subscribe(param => {
+      this.gameId = param.gameId;
+      this.loadGame();
+    });
   }
 
+  private loadGame(): void {
+    if (this.gameSubs) {
+      this.gameSubs.unsubscribe();
+    }
+    this.gameSubs = this.gameService.initGame().subscribe(value => {
+      this.game = new GameDto(value);
+    }, error => {
+      this.router.navigate([`./not-found`]);
+    });
+  }
+
+  onSubmit(): void {
+    if (this.gameCheckSubs) {
+      this.gameCheckSubs.unsubscribe();
+    }
+    this.gameCheckSubs = this.gameService.check(this.gameId, this.number).subscribe(value => {
+      this.result = new ResultDto(value);
+      this.checkResult();
+    });
+  }
+
+  private checkResult(): void {
+    this.resetErrors();
+    if (this.result.result === 'EQUAL') {
+      this.equal = true;
+    }
+    if (this.result.result === 'GREATER') {
+      this.greater = true;
+    }
+    if (this.result.result === 'LOWER') {
+      this.lower = true;
+    }
+  }
+
+  private resetErrors(): void {
+    this.greater = false;
+    this.lower = false;
+  }
 }
